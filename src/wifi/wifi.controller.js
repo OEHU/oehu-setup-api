1903935808fcae6be5c74ed13264ec9706af0c07
Mick de Graaf <mick@mickdegraaf.nl>
2018-09-30 17:13:19 +0200

    typo

--- src/wifi/wifi.controller.js
+++ src/wifi/wifi.controller.js
@@ -4,6 +4,6 @@ const util = require('util');
 const wifiScan = util.promisify(piWifi.scan);
 
 exports.scan = async (req, res) => {
-  let networks = await wifiScan());
+  let networks = await wifiScan();
   res.json({result: networks});
 }
